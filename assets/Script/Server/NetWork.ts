6c6a8c978b5fbf4abb4714ba90cac6b5564b27e3
qinsan-6 <960357393@qq.com>
2024-02-14 00:06:03 +0800

    提交

---
+++ assets/Script/Server/NetWork.ts
@@ -0,0 +1,14 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('NetWork')
+export class NetWork extends Component {
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+        
+    }
+}
+
---
+++ assets/Script/Types.ts
@@ -0,0 +1,14 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('Types')
+export class Types extends Component {
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+        
+    }
+}
+
